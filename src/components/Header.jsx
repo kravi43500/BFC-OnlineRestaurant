158056ac7a7568e82bb396e26ae403d233329138
Ravi Kumar <kravi4350@gmail.com>
2022-10-03 13:21:52 +0530

    Completeing route Page

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -55,7 +55,7 @@ const Header = () => {
     <header className="fixed z-50 w-screen p-3 px-4 md:p-6 md:px-16 bg-primary">
       {/* desktop & tablet */}
       <div className="hidden md:flex w-full h-full items-center justify-between">
-        <Link to={"/"} className="flex items-center gap-2">
+        <Link to={"/BFC-OnlineRestaurant/"} className="flex items-center gap-2">
           <img src={Logo} className="w-8 object-cover" alt="logo" />
           <p className="text-headingColor text-xl font-bold"> BFC</p>
         </Link>
